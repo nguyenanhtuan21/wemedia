71f8da49ebad375d0b16f35a6836050a95f88cba
nguyenanhtuan21 <tuananhnguyenyt@gmail.com>
2025-12-25 16:25:26 +0700

    asdf

---
+++ wemedia/sites/js/home.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+new WOW().init();
+var swiper = new Swiper('.mySwiper', {
+    // autoplay: {
+    //     delay: 30000, // 3 giây
+    //     disableOnInteraction: false, // Không tắt autoplay khi người dùng tương tác
+    // }
+});
+
+}
